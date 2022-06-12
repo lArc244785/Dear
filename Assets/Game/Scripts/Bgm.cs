7c54e0145a532539902715d07de163f19aeed11c
lArc244785 <jlky8932@gmail.com>
2022-06-12 14:26:01 +0900

    사운드 업데이트

--- Assets/Game/Scripts/Bgm.cs
+++ Assets/Game/Scripts/Bgm.cs
@@ -4,38 +4,73 @@ using UnityEngine;
 
 public class Bgm : MonoBehaviour
 {
+    public enum BgmType
+    {
+        Forest, Factory, Total
+    }
     [SerializeField]
-    private FMODUnity.EventReference m_bgmEvent;
+    private BgmType m_currentBgm;
+
+
+    [SerializeField]
+    private FMODUnity.EventReference[] m_bgmEvents;
     [SerializeField]
     private string m_prograssID;
     [SerializeField]
     private string m_hitIndexID;
 
-    private FMOD.Studio.EventInstance m_bgmInstance;
-    private FMOD.Studio.PARAMETER_ID m_parameterPrograss;
-    private FMOD.Studio.PARAMETER_ID m_parameterHitIndexID;
+    private FMOD.Studio.EventInstance[] m_bgmInstances;
+    private FMOD.Studio.PARAMETER_ID[] m_parameterPrograss;
+    private FMOD.Studio.PARAMETER_ID[] m_parameterHitIndexIDs;
 
     public void Init()
     {
-        m_bgmInstance = FMODUnity.RuntimeManager.CreateInstance(m_bgmEvent);
-        SoundManager.instance.GetID(m_bgmInstance, m_prograssID, out m_parameterPrograss);
-        SoundManager.instance.GetID(m_bgmInstance, m_hitIndexID, out m_parameterHitIndexID);
+        m_bgmInstances = new FMOD.Studio.EventInstance[(int)BgmType.Total];
+        m_parameterPrograss = new FMOD.Studio.PARAMETER_ID[(int)BgmType.Total];
+        m_parameterHitIndexIDs = new FMOD.Studio.PARAMETER_ID[(int)BgmType.Total];
+
+        for (int i = 0; i < m_bgmEvents.Length ; i ++)
+        {
+            m_bgmInstances[i] = FMODUnity.RuntimeManager.CreateInstance(m_bgmEvents[i]);
+            SoundManager.instance.GetID(m_bgmInstances[i], m_prograssID, out m_parameterPrograss[i]);
+            SoundManager.instance.GetID(m_bgmInstances[i], m_hitIndexID, out m_parameterHitIndexIDs[i]);
+        }
+        
+
+
+        BgmStart();
+    }
+
+
+    public void BgmChange(BgmType type)
+    {
+        if (type == m_currentBgm)
+            return;
+
+        BgmStop();
+        m_currentBgm = type;
         BgmStart();
     }
 
     public void BgmStart()
     {
-        SoundManager.instance.SoundPlay(m_bgmInstance);
+        SoundManager.instance.SoundPlay(m_bgmInstances[(int)m_currentBgm]);
     }
+
+    private void BgmStop()
+    {
+        SoundManager.instance.SoundStop(m_bgmInstances[(int)m_currentBgm]);
+    }
+
 
     public void SetParamaterPrograss(float value)
     {
-        m_bgmInstance.setParameterByID(m_parameterPrograss, value);
+        m_bgmInstances[(int)m_currentBgm].setParameterByID(m_parameterPrograss[(int)m_currentBgm], value);
     }
 
     public void SetParamaterHitIndexID(float value)
     {
-        m_bgmInstance.setParameterByID(m_parameterHitIndexID, value);
+        m_bgmInstances[(int)m_currentBgm].setParameterByID(m_parameterHitIndexIDs[(int)m_currentBgm], value);
     }
 
 
